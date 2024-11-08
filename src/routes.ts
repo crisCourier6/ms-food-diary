a8b9f014873a732ea229074ee621a284a89c72dd
Cristobal Vasquez <cristobal.vasquez.m@usach.cl>
2024-11-07 22:02:59 -0300

    routing for nginx fixed

--- src/routes.ts
+++ src/routes.ts
@@ -3,103 +3,103 @@ import { MainController } from "./controller/MainController";
 
 export const Routes = [{
     method: "get",
-    route: "/api/v1/food-diary",
+    route: "/food-diary",
     controller: MainController,
     action: "foodDiaryAll"
 }, {
     method: "get",
-    route: "/api/v1/food-diary/byId/:id",
+    route: "/food-diary/byId/:id",
     controller: MainController,
     action: "foodDiaryOne"
 }, 
 {
     method: "get",
-    route: "/api/v1/food-diary/byUser/:userId",
+    route: "/food-diary/byUser/:userId",
     controller: MainController,
     action: "foodDiaryOneByUser"
 },{
     method: "post",
-    route: "/api/v1/food-diary",
+    route: "/food-diary",
     controller: MainController,
     action: "foodDiarySave"
 }, {
     method: "delete",
-    route: "/api/v1/food-diary/byId/:id",
+    route: "/food-diary/byId/:id",
     controller: MainController,
     action: "foodDiaryRemove"
 }, {
     method: "patch",
-    route: "/api/v1/food-diary/byId/:id",
+    route: "/food-diary/byId/:id",
     controller: MainController,
     action: "foodDiaryUpdate"
 }, {
     method: "delete",
-    route: "/api/v1/food-diary/byUser/:userId",
+    route: "/food-diary/byUser/:userId",
     controller: MainController,
     action: "foodDiaryRemoveByUser"
 }, {
     method: "get",
-    route: "/api/v1/food-diary-google/:token",
+    route: "/food-diary-google/:token",
     controller: MainController,
     action: "foodDiaryAllGoogle"
 }, {
     method: "post",
-    route: "/api/v1/food-diary-google/:token",
+    route: "/food-diary-google/:token",
     controller: MainController,
     action: "foodDiarySaveGoogle"
 }, {
     method: "delete",
-    route: "/api/v1/food-diary-google/:token",
+    route: "/food-diary-google/:token",
     controller: MainController,
     action: "foodDiaryRemoveGoogle"
 }, {
     method: "get",
-    route: "/api/v1/diary-entry-google/:token/:id",
+    route: "/diary-entry-google/:token/:id",
     controller: MainController,
     action: "diaryEntryAllByDiaryGoogle"
 }, {
     method: "post",
-    route: "/api/v1/diary-entry-google/:token/:id",
+    route: "/diary-entry-google/:token/:id",
     controller: MainController,
     action: "diaryEntrySaveGoogle"
 }, {
     method: "patch",
-    route: "/api/v1/diary-entry-google/:token/:id",
+    route: "/diary-entry-google/:token/:id",
     controller: MainController,
     action: "diaryEntryUpdateGoogle"
 }, {
     method: "delete",
-    route: "/api/v1/diary-entry-google/:token/:id",
+    route: "/diary-entry-google/:token/:id",
     controller: MainController,
     action: "diaryEntryRemoveByIdGoogle"
 }, 
 {
     method: "get",
-    route: "/api/v1/food-diary/byId/:id/entries",
+    route: "/food-diary/byId/:id/entries",
     controller: MainController,
     action: "diaryEntryAll"
 }, 
 {
     method: "get",
-    route: "/api/v1/food-diary/byId/:id/entries/:entryId",
+    route: "/food-diary/byId/:id/entries/:entryId",
     controller: MainController,
     action: "diaryEntryOne"
 },
 {
     method: "post",
-    route: "/api/v1/food-diary/byId/:id/entries",
+    route: "/food-diary/byId/:id/entries",
     controller: MainController,
     action: "diaryEntryCreate"
 },
 {
     method: "patch",
-    route: "/api/v1/food-diary/byId/:id/entries/:entryId",
+    route: "/food-diary/byId/:id/entries/:entryId",
     controller: MainController,
     action: "diaryEntryUpdate"
 },
 {
     method: "get",
-    route: "/api/v1/food-diary/byId/:id/entries/:entryId",
+    route: "/food-diary/byId/:id/entries/:entryId",
     controller: MainController,
     action: "diaryEntryRemove"
 },]
